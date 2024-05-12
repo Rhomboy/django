ec21f7b35bf65369592a59083ee7ea4ef0dffe65
Rhomboy <123803643+Rhomboy@users.noreply.github.com>
2024-05-12 18:13:32 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -51,8 +51,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleWare'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'mysite.urls'
