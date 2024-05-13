4745bc3d02bbf4b44de8b8236e7011a730216220
Rhomboy <123803643+Rhomboy@users.noreply.github.com>
2024-05-13 15:00:15 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 urlpatterns = [
     #path("", views.starting_page, name="starting-page"),
    # path("posts/", views.posts_page, name="posts-page"),
-    path("posts/<slug:slug>", views.PostView.as_view(), name="post-detail-page"),
+    path("posts/<slug:slug>", views.PostView.as_view(), name="post-detail"),
     path("all-posts/", views.All_PostsView.as_view(), name="posts-page"),
     path("", views.StartingPageView.as_view(), name="starting-page"),
     path("read-later", views.ReadLater.as_view(), name="read-later")
